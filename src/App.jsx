6a14d1c6f5f33c41a401cc5b394b67205f697bcf
Alex <bettalex33@gmail.com>
2026-04-08 01:19:52 -0500

    point chat to Railway deployment

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ export default function App() {
     completeMission(1)
 
     try {
-      const res = await fetch('http://127.0.0.1:5000/chat', {
+      const res = await fetch('https://kinu-web-production.up.railway.app/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: userMsg, history: [] })
